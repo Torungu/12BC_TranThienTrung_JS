6e4a3de7790f1f1e42f98d234b8e6f10fc1799dd
TranThienTrung <xxdarkblade2906@gmail.com>
2024-06-06 11:55:52 +0700

    js

--- B_6/index.js
+++ B_6/index.js
@@ -3,6 +3,7 @@
 //     console.log(`District${buocNhay}`)
 //     buocNhay++
 // }
+//              Tổng các số chẵn
 document.querySelector('#button1').onclick=()=>{
     let soN = document.querySelector('#soN').value*1
     let sum=0
@@ -20,6 +21,7 @@ document.querySelector('#button1').onclick=()=>{
     document.querySelector('#result1').value=`Tổng các số chẵn từ 0 đến ${soN} là ${sum}`
 }
 
+//              Đếm số chia hết cho 3
 document.querySelector('#button2').onclick=()=>{
     let soN1=document.querySelector('#soN1').value*1
     let count=0
@@ -33,4 +35,60 @@ document.querySelector('#button2').onclick=()=>{
     while (i<=soN1)
     document.querySelector('#result2').value=`Có ${count} số chia hết cho 3 từ 0 đến ${soN1}`
 }
-    
+
+//              Tạo layout HTML
+// ----------------- Cách1 -------------------
+// document.querySelector('#button3').onclick=()=>{
+// let content="";
+// for(let i=1;i<=10;i++){
+//     if(i%2===1){
+//         content+="<div class='bg-danger text-white text-center fw-bold p-3'>Lẻ</div>"
+//     }
+//     else{
+//         content+="<div class='bg-primary text-white text-center fw-bold p-3'>Chẵn</div>"
+//     }
+//      document.querySelector('#baiTap1').innerHTML=content
+// }
+// }
+
+
+// ----------------- Cách2 -------------------
+document.querySelector('#button3').onclick=()=>{
+    let baiTap=document.querySelector('#baiTap1')
+    for(let i=1;i<=10;i++){
+        let renderDiv=document.createElement("div")
+        if(i % 2==1){
+            renderDiv.className='bg-danger p-3'
+        }
+        else{
+            renderDiv.className='bg-primary p-3'
+        }
+        baiTap.appendChild(renderDiv)
+    }
+}
+
+//              Tạo ô vuông và tam giác
+document.querySelector('#button4').onclick=()=>{
+    let dongNgoiSao='* * * * *'
+    let ketQua=''
+    for(i=0;i<=4;i++){
+        ketQua+=dongNgoiSao+"<br>"
+    }
+    document.querySelector('#result4').innerHTML=ketQua
+}
+
+
+let taoTamGiac=(soDong)=>{
+    let ketQua=''
+    for(i=0;i<=soDong-1;i++){
+        let dongNgoiSao=''
+        for(z=0;z<=i;z++){
+            dongNgoiSao+="* "
+        }
+        ketQua+=dongNgoiSao+'<br>'
+    }
+    return ketQua
+}
+document.querySelector('#button5').onclick=()=>{
+    document.querySelector('#result4').innerHTML=taoTamGiac(5)
+}
